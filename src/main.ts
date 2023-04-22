a81b47ab15f2230bea2efbc8854406b7fb167a25
DevSazal <sazal836@gmail.com>
2023-04-22 15:15:59 +0200

    initial nestjs backend

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('v1');
+
+  // port initialize
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
+}
+bootstrap();
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/v1 (GET)', async () => {
+    return await request(app.getHttpServer()).get('/v1').expect(404);
+  });
+});
